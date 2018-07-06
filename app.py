6566802e03a5a453766dd4149c5ca2b255cbe74a
huzeyfe798 <huzeyfeulutas798@gmail.com>
2018-07-06 10:59:14 +0300

    add homepage favorite toberead html page files and add navbar all files and fav-app-angular get and route interpolate problem solved add table in favorite html and take data from json file to table

--- app.py
+++ app.py
@@ -7,6 +7,23 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/favorite')
+def favorite():
+    return render_template('favorite.html')
+
+@app.route('/home')
+def homepage():
+    return render_template('homepage.html')
+
+@app.route('/toberead')
+def toberead():
+    return render_template('toberead.html')
+
+
+@app.route('/addbook')
+def adbook():
+    return render_template('index.html')
+
 @app.route('/lang',methods=['POST'])
 def addOne():
     name1 = {'bookname' : request.json['bookname'],'authorname' : request.json['authorname'],'year' : request.json['year'],'favorite' : request.json['favorite'],'read' : request.json['read'],'toberead' : request.json['toberead']}
@@ -18,9 +35,15 @@ def addOne():
         json.dump(books,outputfile)
     return jsonify({'names':name1})
 
-# @app.route('/lang',methods=['GET'])
-# def returnAll():
-#     return jsonify({'names':name1})
+@app.route('/jsondatas',methods=['GET'])
+def takeAll():
+
+    with open('database.json') as inputfile:
+        books1 = json.load(inputfile)
+
+    return jsonify(books1)
+
+
 
 
 if __name__ == '__main__':
--- static/js/flaskAng.js
+++ static/js/flaskAng.js
@@ -1,5 +1,8 @@
 var app = angular.module("myApp",[]);
 
+app.config(function($interpolateProvider) {
+    $interpolateProvider.startSymbol('//').endSymbol('//');
+});
 
 
 app.controller("myCtrl", function($scope, $http){
@@ -17,6 +20,23 @@ app.controller("myCtrl", function($scope, $http){
         }
 
     };
+});
+
+
+app.controller("myCtrlFav", function($scope, $http){
+
+        $http.get('/jsondatas').then(successCallback, errorCallback);
+
+        function successCallback(datas){
+          $scope.books = datas.data;
+          console.log(data);
+        }
+        function errorCallback(){
+            console.log("Error");
+
+        }
+
+
 });
 
 
