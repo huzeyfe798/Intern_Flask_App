fbc4af52b32e5c65d894a9a7a4c04add39bd37b3
huzeyfe798 <huzeyfeulutas798@gmail.com>
2018-07-10 17:48:35 +0300

    add google book api and search button connect this button to database to google api add filter if search book exist in my books

--- static/js/flaskAng.js
+++ static/js/flaskAng.js
@@ -23,7 +23,122 @@ app.controller("myCtrl", function($scope, $http){
 
 
 app.controller("myCtrlFav", function($scope, $http){
+        var req_prefix = "https://www.googleapis.com/books/v1/volumes?q=";
 
+
+        $scope.result_books = [];
+
+
+
+    $scope.searchBooks = function(){
+
+            var contr = false;
+            if($scope.query == "" || $scope.query == undefined) {
+
+                return;
+            }
+
+            $("#search-box").hide();
+            $("#loading-box").show();
+
+
+            for(x in $scope.books){
+
+                if($scope.query == $scope.books[x].bookname){
+                  contr = true;
+                }
+            }
+            if(contr){
+                $scope.filterData = function(item){
+                    return item.bookname == $scope.query;
+                }
+                $("#search-box").show();
+                $("#loading-box").hide();
+            }
+            else{
+                var request = req_prefix + $scope.query;
+
+                $http.get(request).then(successCallback, errorCallback);
+                3
+                function successCallback(resp){
+                    if(resp.data.items && resp.data.items.length > 0){
+                        $scope.result_books = resp.data.items;
+                        $("#msg").text("");
+                    }
+                    else{
+                        $("#msg").text("No Results");
+                    }
+
+                    $("#search-box").show();
+                    $("#loading-box").hide();
+
+                }
+                function errorCallback(){
+                    console.log("Error");
+                    $("#search-box").show();
+                    $("#loading-box").hide();
+                }
+
+
+            }
+        }
+
+
+        $scope.clearResults = function() {
+            $scope.result_books = [];
+            $scope.filterData = function(){
+                return true;
+            }
+            $scope.query = '';
+            $("#query").text( "" );
+        }
+
+        $scope.addBook = function(b){
+
+            var create = {};
+            if(b.volumeInfo.hasOwnProperty('imageLinks')){
+                create['imageURL']= b.volumeInfo.imageLinks.thumbnail;
+            }else{
+                create['imageURL']= "";
+            }
+            if(b.volumeInfo.hasOwnProperty('title')){
+                create['bookname']= b.volumeInfo.title;
+            }else{
+                create['bookname']= "";
+            }
+            if(b.volumeInfo.hasOwnProperty('authors')){
+                create['authorname']= b.volumeInfo.authors[0];
+            }else{
+                create['authorname']= "";
+            }
+            if(b.volumeInfo.hasOwnProperty('publishedDate')){
+                create['year']= b.volumeInfo.publishedDate;
+            }else{
+                create['year']= "";
+            }
+            create['read']= "false";
+            create['favorite']= "false";
+            create['toberead']= "true";
+
+            $scope.books.push(create);
+            $http.post("/post",create ).then(successCallback, errorCallback);
+            function successCallback(){
+                console.log("Post successs");
+
+            }
+            function errorCallback(){
+                console.log("Error");
+
+            }
+
+
+
+        }
+
+
+
+
+        // if atılcak
         $http.get('/jsondatas').then(successCallback, errorCallback);
 
         function successCallback(datas){
